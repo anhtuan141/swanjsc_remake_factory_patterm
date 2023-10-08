da0a91270b8d7ac15f5d46fdbece2857f3d47752
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-08 09:42:00 +0700

    Create SupplierFactory

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'alias' => $this->faker->slug,
+            'status' => $this->faker->randomElement([0, 1]),
+        ];
+    }
+}
