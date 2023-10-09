8d8c61d104bf26ae0680d0aea3eec6e6318059db
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-09 22:10:47 +0700

    Create ProductRepositoryInterface

---
+++ app/Interfaces/ProductRepositoryInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Http\Requests\Api\Product\ProductStoreRequest;
+use App\Http\Requests\Api\Product\ProductUpdateRequest;
+use App\Models\Product;
+
+interface ProductRepositoryInterface
+{
+    /**
+     * @param $queryParameter
+     * @return mixed
+     */
+    public function getListProduct($queryParameter);
+
+    /**
+     * @param int $id
+     * @param $queryParameter
+     * @return mixed
+     */
+    public function getByID(int $id, $queryParameter);
+
+    /**
+     * @param $username
+     * @return mixed
+     */
+    public function checkProductExists($username);
+
+    /**
+     * @param ProductStoreRequest $request
+     * @return mixed
+     */
+    public function storeProduct(ProductStoreRequest $request);
+
+    /**
+     * @param ProductUpdateRequest $request
+     * @param Product|null $product
+     * @param int $id
+     * @return mixed
+     */
+    public function updateProduct(ProductUpdateRequest $request, Product $product = null, int $id);
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function destroyUser(int $id);
+
+
+}
