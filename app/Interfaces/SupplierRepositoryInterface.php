6f4c8513fb1b3ad086650a790d8b65d494878df7
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-09 22:25:43 +0700

    Create SupplierRepositoryInterface

---
+++ app/Interfaces/SupplierRepositoryInterface.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Http\Requests\Api\Supplier\SupplierStoreRequest;
+use App\Http\Requests\Api\Supplier\SupplierUpdateRequest;
+use App\Models\Supplier;
+
+interface SupplierRepositoryInterface
+{
+    /**
+     * @param $queryParameter
+     * @return mixed
+     */
+    public function getListSupplier($queryParameter);
+
+    /**
+     * @param int $id
+     * @param $queryParameter
+     * @return mixed
+     */
+    public function getByID(int $id, $queryParameter);
+
+    /**
+     * @param $alias
+     * @return mixed
+     */
+    public function checkSupplierExists($alias);
+
+    /**
+     * @param SupplierStoreRequest $request
+     * @return mixed
+     */
+    public function storeSupplier(SupplierStoreRequest $request);
+
+    /**
+     * @param SupplierUpdateRequest $request
+     * @param Supplier|null $supplier
+     * @param int $id
+     * @return mixed
+     */
+    public function updateSupplier(SupplierUpdateRequest $request, Supplier $supplier = null, int $id);
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function destroySupplier(int $id);
+
+}
