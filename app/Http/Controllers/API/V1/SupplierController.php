169f7c24ff8f4c7e0efabd447c357c929aea1833
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-13 20:32:58 +0700

    Create logic function index() in API\V1\SupplierController

--- app/Http/Controllers/API/V1/SupplierController.php
+++ app/Http/Controllers/API/V1/SupplierController.php
@@ -3,24 +3,43 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\SupplierRepositoryFactoryInterface;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SupplierController extends Controller
 {
     /**
+     * @var SupplierRepositoryFactoryInterface
+     */
+    private $supplierRepositoryFactory;
+
+    /**
+     * @param SupplierRepositoryFactoryInterface $supplierRepositoryFactory
+     */
+    public function __construct(SupplierRepositoryFactoryInterface $supplierRepositoryFactory)
+    {
+        $this->supplierRepositoryFactory = $supplierRepositoryFactory;
+    }
+
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        $supplierList = $this->supplierRepositoryFactory->getListSupplierRepository();
+
+        return response()->json([
+            'data' => $supplierList
+        ], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -31,7 +50,7 @@ class SupplierController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -42,8 +61,8 @@ class SupplierController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -54,7 +73,7 @@ class SupplierController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
